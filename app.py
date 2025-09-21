ec041192b67d74500f2be2c9df719a16a96184a2
mymimy1234 <mymimy1234@gmail.com>
2025-09-21 15:26:53 +0900

    2025.09.21

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 
 from service.getMatchData import getMatchData
+from service.getTierData import GetTier
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,8 +13,10 @@ def search():
     if request.method == 'POST':
         gameName = request.form['gameName']
         tagLine = request.form['tagLine']
-        result = getMatchData(gameName, tagLine)
-        return render_template('search.html', result=result)
+        result,puuid = getMatchData(gameName, tagLine)
+        tier = GetTier(puuid)
+        summoner_name=gameName+"#"+tagLine
+        return render_template('search.html',summoner_name=summoner_name,result=result,tier=tier)
     if request.method == 'GET':
         return render_template('search.html')
 
